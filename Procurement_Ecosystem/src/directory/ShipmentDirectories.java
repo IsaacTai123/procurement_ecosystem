4cdb37bc2b388d94b62a794c84ad5f2f531b103d
linweih-alvin <lin.weih@northeastern.edu>
2025-04-13 17:42:40 -0400

    create fedEx, asus simulated data, get shipment directory from main jframe

---
+++ Procurement_Ecosystem/src/directory/ShipmentDirectories.java
@@ -0,0 +1,29 @@
+package directory;
+
+import enums.EnterpriseType;
+import model.ecosystem.Enterprise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author tisaac
+ */
+public class EnterpriseDirectory {
+
+    private final List<Enterprise> enterprisesList;
+
+    public EnterpriseDirectory() {
+        this.enterprisesList = new ArrayList<>();
+    }
+
+    public Enterprise createEnterprise(String name, EnterpriseType type) {
+        Enterprise enterprise = new Enterprise(name, type);
+        enterprisesList.add(enterprise);
+        return enterprise;
+    }
+
+    public List<Enterprise> getEnterprisesList() {
+        return enterprisesList;
+    }
+}
